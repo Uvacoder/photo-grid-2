538fa37f17313be390baea47e316363fb5a760d9
Matt Lehrer <lehrerm@gmail.com>
2019-11-22 12:30:05 -0800

    add name conflict avoidance and photo resizing

--- grid.js
+++ grid.js
@@ -1,40 +1,47 @@
 const fs = require('fs');
-const { spawnSync } = require('child_process');
-
+const { execSync } = require('child_process');
+const shortid = require('shortid');
+ 
 const directoryWithPics = './photos';
-const numPicsPerRow = 25;
+const numPicsPerRow = 50;
+const resolutionInGrid = '640x480';
+const gridID = shortid.generate();
 
-const findAllFiles = dir => {
+const findAllPhotos = dir => {
   var results = [];
   var list = fs.readdirSync(dir);
   list.forEach((file) => {
       file = dir + '/' + file;
       var stat = fs.statSync(file);
       if (stat && stat.isDirectory()) { 
-          results = results.concat(findAllFiles(file));
+          results = results.concat(findAllPhotos(file));
       } else { 
-          if (file.endsWith('.jpg')) results.push(file);
+          if (file.toLowerCase().endsWith('.jpg')) {
+            results.push(file);
+          } else {
+            console.log(`Skipping ${file}`);
+          }
       }
   });
   return results;
 }
 
-const files = findAllFiles(directoryWithPics);
+const photosArray = findAllPhotos(directoryWithPics);
 
-console.log(`Generating rows with ${numPicsPerRow} photos each for ${files.length} total photos`);
+console.log(`Starting script with id ${gridID} to avoid name conflicts.`)
+console.log(`Generating ${Math.ceil(photosArray.length/numPicsPerRow)} rows with max ${numPicsPerRow} photos each for ${photosArray.length} total photos...`);
 
 let i = 0;
-while (i < files.length) {
-  const lastPicInRow = i + numPicsPerRow < files.length 
-    ? i + numPicsPerRow 
-    : files.length - 1;
-  const filesInRow = files.slice(i,lastPicInRow);
-  const rowNum = String(i/numPicsPerRow).padStart(4, '0');
-  const args = [...filesInRow, '+append', `row${rowNum}.jpg`];
-  console.log(`Generating row ${rowNum+1}`);
-  spawnSync('magick', args);
+while (i < photosArray.length) {
+  const photosInRow = photosArray.slice(i, i + numPicsPerRow);
+  const photosInRowWithNewResolution = photosInRow.map(filename => `'${filename}[${resolutionInGrid}]'`);
+  const rowNum = String(i/numPicsPerRow).padStart(3, '0');
+  const args = [...photosInRowWithNewResolution, '+append', `grid_${gridID}_row_${rowNum}.jpg`];
+  console.log(`Generating row ${rowNum}`);
+  execSync(`magick ${args.join(' ')}`);
   i += numPicsPerRow;
 }
 
-console.log('finished with rows, combining into grid...');
-spawnSync('magick', ['row*.jpg', '-append', `grid${numPicsPerRow}_per_row.jpg`]);
+console.log('Finished with rows, combining into grid...');
+spawnSync('magick', [`grid_${gridID}_row_*.jpg`, '-append', `grid_${gridID}_${numPicsPerRow}_per_row.jpg`]);
+console.log('Done.')
