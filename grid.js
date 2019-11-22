8e13239902ca89f4bda9397007deb8ce76e5bbd3
Matt Lehrer <lehrerm@gmail.com>
2019-11-22 12:54:18 -0800

    move output to a directory

--- grid.js
+++ grid.js
@@ -6,29 +6,34 @@ const directoryWithPics = './photos';
 const numPicsPerRow = 50;
 const resolutionInGrid = '640x480';
 const gridID = shortid.generate();
+const gridDirectory = `./grid_${gridID}`;
 
 const findAllPhotos = dir => {
   var results = [];
   var list = fs.readdirSync(dir);
   list.forEach((file) => {
-      file = dir + '/' + file;
-      var stat = fs.statSync(file);
-      if (stat && stat.isDirectory()) { 
-          results = results.concat(findAllPhotos(file));
-      } else { 
-          if (file.toLowerCase().endsWith('.jpg')) {
-            results.push(file);
-          } else {
-            console.log(`Skipping ${file}`);
-          }
+    file = dir + '/' + file;
+    var stat = fs.statSync(file);
+    if (stat && stat.isDirectory()) { 
+      console.log(`Searching ${file}`)
+      results = results.concat(findAllPhotos(file));
+    } else { 
+      if (file.toLowerCase().endsWith('.jpg')) {
+        results.push(file);
+      } else {
+        console.log(`Skipping ${file}`);
       }
+    }
   });
   return results;
 }
 
+console.log(`Starting script with id ${gridID} to avoid name conflicts. Creating directory ${gridDirectory} for new files.`);
+fs.mkdirSync(gridDirectory);
+
+console.log('Finding all photos...');
 const photosArray = findAllPhotos(directoryWithPics);
 
-console.log(`Starting script with id ${gridID} to avoid name conflicts.`)
 console.log(`Generating ${Math.ceil(photosArray.length/numPicsPerRow)} rows with max ${numPicsPerRow} photos each for ${photosArray.length} total photos...`);
 
 let i = 0;
@@ -36,12 +41,13 @@ while (i < photosArray.length) {
   const photosInRow = photosArray.slice(i, i + numPicsPerRow);
   const photosInRowWithNewResolution = photosInRow.map(filename => `'${filename}[${resolutionInGrid}]'`);
   const rowNum = String(i/numPicsPerRow).padStart(3, '0');
-  const args = [...photosInRowWithNewResolution, '+append', `grid_${gridID}_row_${rowNum}.jpg`];
+  const args = [...photosInRowWithNewResolution, '+append', `${gridDirectory}/row_${rowNum}.jpg`];
   console.log(`Generating row ${rowNum}`);
   execSync(`magick ${args.join(' ')}`);
   i += numPicsPerRow;
 }
 
 console.log('Finished with rows, combining into grid...');
-spawnSync('magick', [`grid_${gridID}_row_*.jpg`, '-append', `grid_${gridID}_${numPicsPerRow}_per_row.jpg`]);
+const args = [`${gridDirectory}/row_*.jpg`, '-append', `${gridDirectory}/grid_with_${numPicsPerRow}_per_row.jpg`];
+execSync(`magick ${args.join(' ')}`);
 console.log('Done.')
